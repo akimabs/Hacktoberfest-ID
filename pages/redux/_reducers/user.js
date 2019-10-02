5a00f78a25f5404b97c218846cc53e1a776293b3
danivickym <danivickymdr@gmail.com>
2019-10-02 21:40:03 +0700

    user 3 error

--- pages/redux/_reducers/user.js
+++ pages/redux/_reducers/user.js
@@ -8,7 +8,15 @@ const initialState = {
     ],
     error: null
 }
-
+const initialState = {
+    isLoading: true,
+    contributor: [
+        {
+            name: 'Dani PPiki mHanedra'
+        },
+    ],
+    error: null
+}
 const user = (state = initialState, action) => {
     switch (action.type) {
 
