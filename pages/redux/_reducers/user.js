02586ee0341636d46a1055f767c759440b0f50bc
danivickym <danivickymdr@gmail.com>
2019-10-02 21:40:44 +0700

    Update dvm name

--- pages/redux/_reducers/user.js
+++ pages/redux/_reducers/user.js
@@ -8,7 +8,15 @@ const initialState = {
     ],
     error: null
 }
-
+const initialState = {
+    isLoading: true,
+    contributor: [
+        {
+            name: 'Tolong PullKan'
+        },
+    ],
+    error: null
+}
 const user = (state = initialState, action) => {
     switch (action.type) {
 
